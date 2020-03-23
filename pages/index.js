6716b52dc6fb58b9306df39af5601398cc8c5afb
patheticGeek <shivamnagi14@gmail.com>
2020-03-23 17:45:05 +0530

    nav perfected

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 function Index() {
   return (
-    <main>
+    <>
       <header>
         <div className="content">
           <h3 className="header-content__subtitle">Web & Mobile developer</h3>
@@ -21,8 +21,43 @@ function Index() {
           <a className="header-content__btn-next" />
         </Link>
       </header>
-      <section id="home-about"></section>
-    </main>
+
+      <section id="home-about">
+        <div className="content">
+          <div className="about-text">
+            <div className="about-text__title">What can i do for you?</div>
+            <div className="about-text__body">
+              I can help convert designs into well built apps which are equally performant as their design.
+            </div>
+          </div>
+
+          <div className="about-services">
+            <div className="about-services-col">
+              <h2 className="about-services__title">Web</h2>
+              <ul className="about-services__list">
+                <li>HTML5/CSS3/JS</li>
+                <li>Node.js</li>
+                <li>Express</li>
+                <li>Next</li>
+                <li>React</li>
+                <li>Redux</li>
+              </ul>
+            </div>
+
+            <div className="about-services-col">
+              <h2 className="about-services__title">Mobile</h2>
+              <ul className="about-services__list">
+                <li>XCode</li>
+                <li>Android Studio</li>
+                <li>React Native</li>
+                <li>React Native Navigation</li>
+                <li>RN Firebase</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 }
 
