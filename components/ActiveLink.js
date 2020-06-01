71cf52a27cc50728b11483f584792a4cdd508520
Pathetic Geek <geekpathetic@gmail.com>
2020-06-01 22:34:24 +0530

    proptypes added

--- components/ActiveLink.js
+++ components/ActiveLink.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { useRouter } from "next/router";
+import PropTypes from "prop-types";
 
 function ActiveLink({ children, href, className, activeClassName }) {
   const router = useRouter();
   if (!className) className = "";
   if (!activeClassName) activeClassName = "";
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     e.preventDefault();
     router.push(href);
   };
@@ -21,5 +22,12 @@ function ActiveLink({ children, href, className, activeClassName }) {
     </a>
   );
 }
+
+ActiveLink.propTypes = {
+  children: PropTypes.any,
+  href: PropTypes.string,
+  className: PropTypes.string,
+  activeClassName: PropTypes.string,
+};
 
 export default ActiveLink;
